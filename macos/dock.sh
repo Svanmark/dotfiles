ddcb2d1f7b2810ec0b45ae54c1256192bd58e002
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2023-02-27 23:24:00 +0100

    Fix terminal in dock

--- macos/dock.sh
+++ macos/dock.sh
@@ -2,7 +2,7 @@
 
 dockutil --no-restart --remove all
 dockutil --no-restart --add "/Applications/Firefox.app"
-dockutil --no-restart --add "/Applications/Terminal.app"
+dockutil --no-restart --add "/System/Applications/Utilities/Terminal.app"
 dockutil --no-restart --add "/Applications/Visual Studio Code.app"
 dockutil --no-restart --add "/Applications/Mail.app"
 dockutil --no-restart --add "/Applications/Calendar.app"
