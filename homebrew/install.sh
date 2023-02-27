d5ec8b3e5cea030b98d4ee03b290b6474fcfaea3
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2023-02-27 22:52:02 +0100

    Fix homebrew install

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -2,6 +2,9 @@
 
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/$USER/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 brew update
 brew upgrade
 
@@ -29,8 +32,12 @@ brew install --cask --appdir="/Applications" caffeine
 brew install --cask --appdir="/Applications" visual-studio
 brew install --cask --appdir="/Applications" visual-studio-code
 
+
 # Quick Look plugins
 brew install --cask qlcolorcode qlstephen qlmarkdown quicklook-json quicklook-csv betterzipql qlimagesize webpquicklook qlvideo
+
+# Hyper theme
+hyper i hyper-one-dark
 
 sleep 1
 echo "Success! Brew additional applications are installed."
