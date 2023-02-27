4b59afdef6da920d45cce34a1405293dab2c1c7a
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2023-02-27 23:23:02 +0100

    Fix dock

--- macos/dock.sh
+++ macos/dock.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 dockutil --no-restart --remove all
-dockutil --no-restart --add "/Applications/Finder.app"
 dockutil --no-restart --add "/Applications/Firefox.app"
-dockutil --no-restart --add "/Applications/Hyper.app"
+dockutil --no-restart --add "/Applications/Terminal.app"
 dockutil --no-restart --add "/Applications/Visual Studio Code.app"
 dockutil --no-restart --add "/Applications/Mail.app"
 dockutil --no-restart --add "/Applications/Calendar.app"
@@ -11,8 +10,6 @@ dockutil --no-restart --add "/Applications/Spotify.app"
 dockutil --no-restart --add "/Applications/Slack.app"
 dockutil --no-restart --add "/Applications/Microsoft Outlook.app"
 dockutil --no-restart --add "/Applications/Microsoft Teams.app"
-dockutil --no-restart --add "/Applications/Discord.app"
-
-killall Dock
+dockutil --add "/Applications/Discord.app"
 
 echo "Success! Dock is set."
