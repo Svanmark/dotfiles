b711cd3c97150cf36dd283089305f05b11e599d5
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2023-02-27 23:26:26 +0100

    Add applicaitons and downloads folder to dock

--- macos/dock.sh
+++ macos/dock.sh
@@ -10,6 +10,8 @@ dockutil --no-restart --add "/Applications/Spotify.app"
 dockutil --no-restart --add "/Applications/Slack.app"
 dockutil --no-restart --add "/Applications/Microsoft Outlook.app"
 dockutil --no-restart --add "/Applications/Microsoft Teams.app"
-dockutil --add "/Applications/Discord.app"
+dockutil --no-restart --add "/Applications/Discord.app"
+dockutil --no-restart --add '/Applications' --view grid --display folder --sort name --section others --position 1 --allhomes
+dockutil --add '/Downloads' --view list --display folder --sort name --section others --position end --allhomes
 
 echo "Success! Dock is set."
